f193737430a6c0bbeecffd9a1d9389636e6f869b
DSCoderman <dsenour6@gmail.com>
2023-04-29 16:06:41 -0400

    testing to get category route to work

--- models/index.js
+++ models/index.js
@@ -6,11 +6,13 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignkey: 'category_id'
+  foreignkey: 'category_id',
+  
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
