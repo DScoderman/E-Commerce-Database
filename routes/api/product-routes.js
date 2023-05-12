ea1b91f08b1e45032778afa1ecd88ebd39a4457d
DSCoderman <dsenour6@gmail.com>
2023-05-11 21:52:52 -0400

    fixed bug that wouldn't allow me to pull a single product when testing in insomnia

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
     });
    
 
-  res.status(200).json(travellerData);
+  res.status(200).json(productData);
 } catch (err) {
   res.status(500).json(err);
 }
